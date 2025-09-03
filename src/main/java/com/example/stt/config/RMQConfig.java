87c421ee02b954855e2a59b400c41af04c8c67eb
yogesh pathak <pathaks.hit@gmail.com>
2025-09-03 15:48:53 +0530

    RMQ settings updated inot config and application properties file

--- src/main/java/com/example/stt/config/RMQConfig.java
+++ src/main/java/com/example/stt/config/RMQConfig.java
@@ -8,64 +8,74 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.client.RestTemplate;
 
 import com.google.api.client.util.Value;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-import jakarta.websocket.CloseReason;
-
+@Configuration
 @Component
 public class RMQConfig {
 
+	private static final Logger log = LoggerFactory.getLogger(RMQConfig.class);
+
 	private static final String RABBITMQ_EXCHANGE_NAME = null;
-	
+
 	@Value("${genesys.rmq.hostname}")
 	private String hostName;
-	
+
 	@Value("${genesys.rmq.username}")
 	private String username;
-	
+
 	@Value("${genesys.rmq.password}")
 	private String password;
-	
-	@Autowired
+
 	private Connection rabbitConnection;
-	
-	@Autowired
+
 	private Channel rabbitChannel;
-	
+
 	@Bean
-	public Channel connectToMQ(WebSocketSession session, Map<String, List<String>> headers)
+	public Channel connectToMQ(Map<String, List<String>> headers)
 			throws IOException, KeyManagementException, NoSuchAlgorithmException, URISyntaxException {
 		try {
+			this.rabbitChannel = rabbitConnection.createChannel();
+			this.rabbitChannel.exchangeDeclare(RABBITMQ_EXCHANGE_NAME, "direct", true);
+			System.out.println("RabbitMQ connection and channel established.");
+
+		} catch (IOException e) {
+			log.error("error occured while connecting to RMQ");
+		}
+		return rabbitChannel;
+	}
+
+	@Bean
+	public Connection rabbitConnection() {
+
+		try {
 			ConnectionFactory factory = new ConnectionFactory();
-//			factory.setHost(hostName);
 			factory.setUsername(username);
 			factory.setPassword(password);
 			factory.setUri(hostName);
-
 			this.rabbitConnection = factory.newConnection();
-			this.rabbitChannel = rabbitConnection.createChannel();
-
-			this.rabbitChannel.exchangeDeclare(RABBITMQ_EXCHANGE_NAME, "direct", true);
-
-//			String sessionId = headers.get("Audiohook-Session-Id").get(0);
-//			session.getAttributes().put("sessionId", sessionId);
-
-			System.out.println("RabbitMQ connection and channel established.");
-			
-		} catch (IOException | TimeoutException e) {
-			System.err.println("Error connecting to RabbitMQ: " + e.getMessage());
-			session.close(new CloseStatus(CloseReason.CloseCodes.UNEXPECTED_CONDITION.getCode(),
-					"RabbitMQ connection error."));
+		} catch (IOException | KeyManagementException | NoSuchAlgorithmException | URISyntaxException
+				| TimeoutException e) {
+			log.error("Exception occured while creating rabbitConnection");
+			e.printStackTrace();
 		}
-		return rabbitChannel;
+
+		return rabbitConnection;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+
+		return new RestTemplate();
 	}
 }
